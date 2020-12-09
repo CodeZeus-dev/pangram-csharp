24602e29af87b079f51684a55212c9b36b145cca
CodeZeus-dev <constantine.akritides@gmail.com>
2020-12-09 23:20:46 +0000

    Initial check-in of module pangram-csharp

---
+++ Pangram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        input = input.Replace(" ", String.Empty);
+        char[] inputArray = input.ToCharArray(0, input.Length);
+        char[] alphabet = new char[]
+        {
+          'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+          'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };
+
+        int count = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (alphabet.Contains(Char.ToLower(inputArray[i])))
+            {
+                count += 1;
+            }
+        }
+
+        if (count == inputArray.Length && count != 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
