17c10ea2594d9fa5079c44d802244d034c0ce369
CodeZeus-dev <constantine.akritides@gmail.com>
2020-12-09 23:59:26 +0000

    Updated Pangram.cs so that it passes all tests

--- Pangram.cs
+++ Pangram.cs
@@ -1,32 +1,32 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
         input = input.Replace(" ", String.Empty);
-        char[] inputArray = input.ToCharArray(0, input.Length);
+        char[] inputArray = input.ToLower().ToCharArray(0, input.Length);
+
         char[] alphabet = new char[]
         {
           'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
           'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
         };
 
-        int count = 0;
+        IEnumerable<char> uniqueItems = inputArray.Distinct<char>();
 
-        for (int i = 0; i < inputArray.Length; i++)
+        int count = 0;
+        for (int i = 0; i < string.Join("", uniqueItems).Length; i++)
         {
-            if (alphabet.Contains(Char.ToLower(inputArray[i])))
+            if (alphabet.Contains(string.Join("", uniqueItems)[i]))
             {
                 count += 1;
             }
         }
 
-        if (count == inputArray.Length && count != 0)
-        { 
-            return true;
-        }
+        if (count == 26) { return true; }
 
         return false;
     }
